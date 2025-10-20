3c03730b7c9d756548456a8c3ce7db65a905028e
Joleen <12113@holbertonstudents.com>
2025-10-20 06:15:32 +0000

    update

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
 * main - prints all possible combinations of
 * single-digit numbers, separated by a comma,
-* followed by a new line.
+* followed by a new line at the end.
 *Return: 0 on success
 */
 
@@ -12,15 +12,14 @@ int main(void)
 int i;
 int j;
 for (i = 0; i < 10; i++)
-for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
-putchar(j + '0');
-if (!(i == 8 && j == 9))
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
