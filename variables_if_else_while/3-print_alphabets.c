df47c4f80f6b2d3405e42717deb5fb4f8de61d3a
Joleen <12113@holbertonstudents.com>
2025-10-20 04:23:13 +0000

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints every letter of the alphabet in lowercases
+* and then uppercases, followed by a new line
+*Return: 0 on success
+*/
+
+int main(void)
+{
+int i;
+int j;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (j = 65; j <= 90; j++)
+{
+putchar(j);
+putchar('\n');
+}
+return (0);
+}
