d944f97ec8f62c1400226b7622069c220e1264d3
Joleen <12113@holbertonstudents.com>
2025-10-20 04:03:23 +0000

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+\ **
+*main - prints every letter of the alphabet in a new line
+*
+*Return : 0 on success
+**/
+
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
