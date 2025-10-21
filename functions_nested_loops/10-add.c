32abbf32fd748b1c3205198521128ad677bfbb5a
Joleen <12113@holbertonstudents.com>
2025-10-21 10:20:49 +0000

    update

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 
 /**
 * add - adds two integers and returns the result.
-*
+* @a - placeholder for number
+* @b - placeholder for another number
+* Return: Result of calculation
 */
 int add(int a, int b)
 {
