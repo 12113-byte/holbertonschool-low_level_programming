bf01db8eb773bae945895a05f0dc78d04e77c16b
Joleen <12113@holbertonstudents.com>
2025-10-21 08:24:12 +0000

    update

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -18,12 +18,12 @@ for (hour_tens = 0; hour_tens <= 2; hour_tens++)
 {
 for (hour_ones = 0; hour_ones <= 3; hour_ones++)
 {
-_putchar(hour_tens + '0');
-_putchar(hour_ones + '0');
 for (minute_tens = 0; minute_tens <= 5; minute_tens++)
 {
 for (minute_ones = 0; minute_ones <= 9; minute_ones++)
 {
+_putchar(hour_tens + '0');
+_putchar(hour_ones + '0');
 _putchar(':');
 _putchar(minute_tens + '0');
 _putchar(minute_ones + '0');
