e43d6cba89e1d4d6af24fa1364467db2f15008f0
Joleen <12113@holbertonstudents.com>
2025-10-21 09:50:51 +0000

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: 0 on success
+ *
+ */
+
+void times_table(void)
+{
+int row;
+int col;
+int number;
+
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 0; col++)
+{
+number = row * col;
+if (col == 0)
+{
+_putchar(number + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+if (number < 10)
+{
+_putchar(' ');
+_putchar(number = '0');
+}
+else
+{
+_putchar((number / 10) + '0');
+_putchar((number % 10) + '0');
+}
+}
+}
+_putchar('\n');
+}
+}
