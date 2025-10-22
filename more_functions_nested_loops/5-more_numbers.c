7b5f139781f636e9c060323c7cc2a3cf1196f6b0
Joleen <12113@holbertonstudents.com>
2025-10-22 05:34:32 +0000

    update

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,12 @@
 /**
 * more_numbers - prints numbers 0-14 ten times, followed by a
 * new line after each loop.
-* @i: prints 10 times
-* @n: numbers 0-14
 */
 
 void more_numbers(void)
 {
+int i;
+int j;
 for (i = 1; i <= 10; i++)
 {
 for (n = 0; n <= 14; n++)
