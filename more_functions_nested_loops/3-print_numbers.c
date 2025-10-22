dc81f66723016d7e08e5cea6b242a86b2090bb18
Joleen <12113@holbertonstudents.com>
2025-10-22 04:34:02 +0000

    update

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
 * print_numbers - prints numbers 0-9, followed by a new line.
-* 
+*
 */
 
 void print_numbers(void)
 {
 int n;
-for (n = 48; n >= 57; n++)
+for (n = '0'; n <= '9'; n++)
 {
 _putchar(n);
 _putchar('\n');
