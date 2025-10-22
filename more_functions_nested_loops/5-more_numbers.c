904596b450b7a3703140f49f1f26753f98086535
Joleen <12113@holbertonstudents.com>
2025-10-22 05:01:54 +0000

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers 0-14 ten times, followed by a
+* new line after each loop.
+* @i: prints 10 times
+* @n: numbers 0-14
+*/
+
+void more_numbers(void)
+{
+int i;
+int n;
+for (i = '1'; i <= '10'; i++)
+{
+for (n = '0'; n <= '14'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+}
