6f3dfe4ca750850cc3a2fb36e93a3d76fe47572e
Joleen <12113@holbertonstudents.com>
2025-10-22 04:03:39 +0000

    Task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase characters.
+* Return: 1 on success, otherwise 0.
+*/
+
+int _isupper(int c)
+{
+int result;
+if (c >= 'A' && c <= 'Z')
+{
+result = 1
+}
+else
+{
+result = 0
+}
+_putchar(c);
+_putchar(':');
+return (result);
+}
