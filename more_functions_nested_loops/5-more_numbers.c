cb3cea1745aa64141e97d515683a0ada469ef741
Joleen <12113@holbertonstudents.com>
2025-10-22 05:04:45 +0000

    update

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 {
 int i;
 int n;
-for (i = '1'; i <= '10'; i++)
+for (i = 1; i <= 10; i++)
 {
-for (n = '0'; n <= '14'; n++)
+for (n = 0; n <= 14; n++)
 {
 _putchar(n);
 }
