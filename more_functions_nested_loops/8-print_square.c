c17752bc5f719228b97aafe256e1e2569adf4cc7
Joleen <12113@holbertonstudents.com>
2025-10-23 10:44:55 +0000

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of #'s
+ *
+ */
+
+void print_square(int size)
+{
+int i;
+int n;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (n = 0; size > n; n++)
+{
+for (i = 0; size > i; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
