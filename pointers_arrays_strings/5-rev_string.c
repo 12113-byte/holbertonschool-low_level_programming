119cd10824558573a6ea0b720c9f05571b84cec2
Joleen <12113@holbertonstudents.com>
2025-10-26 10:33:43 +0000

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: parameter to be checked for size.
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+for (length = 0; *s != '\0'; s++, length++)
+{
+
+}
+return (length);
+}
+
+/**
+ * rev_string - saves the modified string in reverse.
+ *
+ */
+
+void rev_string(char *s)
+{
+int start = 0;
+int end = _strlen(s) - 1;
+char temp;
+
+for (start = 0; start < end; start++)
+{
+temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+end--;
+}
+}
