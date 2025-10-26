99ec68c997a412710688905f0433b55ce820859a
Joleen <12113@holbertonstudents.com>
2025-10-26 11:08:04 +0000

    update

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: parameter to be checked for size.
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+for (length = 0; *s != '\0'; s++, length++)
+{
+
+}
+return (length);
+}
+
+/**
  * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: string
@@ -8,9 +25,12 @@
 
 void puts2(char *str)
 {
-for (; *str != '\0'; str += 2)
+int i;
+int length = _strlen(str);
+
+for (i = 0; i < length ; i += 2)
 {
-_putchar(*str);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
