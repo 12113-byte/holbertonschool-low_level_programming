dda41d55aa79f27369d40e1625d9da34c3a9033a
Joleen <12113@holbertonstudents.com>
2025-10-26 11:22:06 +0000

    Task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: parameter to be checked for size.
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+for (length = 0; *s != '\0'; s++, length++)
+{
+
+}
+return (length);
+}
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ *
+ */
+
+void puts_half(char *str)
+{
+int i;
+int length = _strlen(str);
+
+for (i = (length + 1) / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
