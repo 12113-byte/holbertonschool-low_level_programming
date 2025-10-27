8e9e60d9e1f75181756331a3df7c155dc352e1c3
Joleen <12113@holbertonstudents.com>
2025-10-27 07:31:08 +0000

    update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *str)
 	while (*p != '\0')
 	{
 		if ((*p >= 33 && *p <= 47) || (*p >= 58 && *p <= 64) ||
+		(*p >= 91 && *p <= 96) || (*p >= 123 && *p <= 126) ||
 		(*p == ' ' || *p == '\n'))
 		{
 		capitalize = 1;
@@ -21,6 +22,11 @@ char *cap_string(char *str)
 		if ((*p >= 'a' && *p <= 'z') && capitalize)
 		{
 		*p = *p - 32;
+		capitalize = 0;
+		}
+		else if (*p >= 'A' && *p <= 'Z' && capitalize == 0)
+		{
+		*p = *p + 32;
 		capitalize = 0;
 		}
 		p++;
