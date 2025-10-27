8ae2c8822b9f76e4e2b23c8d8ffb314ab40feada
Joleen <12113@holbertonstudents.com>
2025-10-27 07:17:43 +0000

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ *
+ */
+
+char *cap_string(char *str)
+{
+	char *p = str;
+	int capitalize = 1;
+
+	while (*p != '\0')
+	{
+		if ((*p >= 33 && *p <= 47) || (*p >= 58 && *p <= 64) ||
+		(*p == ' ' || *p == '\n'))
+		{
+		capitalize = 1;
+		}
+		if ((*p >= 'a' && *p <= 'z') && capitalize)
+		{
+		*p = *p - 32;
+		capitalize = 0;
+		}
+		p++;
+	}
+	return (str);
+}
