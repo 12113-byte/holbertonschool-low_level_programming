d5e3bd8c7f8d17f31e3b10f10ccc0433975615d0
Joleen <12113@holbertonstudents.com>
2025-10-27 06:23:40 +0000

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,6 +4,7 @@
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
  * @str: the string that gets changed
+ * Return: prints the string in uppercases
  */
 
 char *string_toupper(char *str)
