4b13d27ddd0051b5c7ec159a018457cb120666c6
Joleen <12113@holbertonstudents.com>
2025-10-30 07:06:19 +0000

    Task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @n: amount of bytes
+ * @dest: memory copied to
+ * @src: memory copied from
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	char *original = dest;
+
+	for (i = 0; i < n; i++)
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+	return (original);
+}
