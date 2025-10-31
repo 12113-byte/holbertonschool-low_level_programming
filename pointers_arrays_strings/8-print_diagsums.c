27a25be7187c8f79ac3118a56a5a45ec06e3a15f
Joleen <12113@holbertonstudents.com>
2025-10-31 09:02:47 +0000

    test

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a swaure matrix of integers
+ *
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
