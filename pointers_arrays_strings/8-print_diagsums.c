4249b8b7ca707cf0a8d4549454f872e7fa20fabe
Joleen <12113@holbertonstudents.com>
2025-10-31 11:31:53 +0000

    Task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -4,11 +4,20 @@
 /**
  * print_diagsums - prints the sum of the two diagonals
  * of a swaure matrix of integers
- *
- *
+ * @a: the poistion of the number in the grid
+ * @size: the size of the grid
  */
 
 void print_diagsums(int *a, int size)
 {
+	int i;
+	int sum_main = 0;
+	int sum_other = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		sum_main += a[(i * size) + i];
+		sum_other += a[(i * size) + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum_main, sum_other);
 }
