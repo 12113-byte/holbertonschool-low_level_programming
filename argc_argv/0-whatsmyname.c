f4251845413a5e3d9eddad45e54fc3d911bf439b
Joleen <12113@holbertonstudents.com>
2025-11-02 10:21:04 +0000

    Task 1:

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - changes the file name
+ * @argc: voided
+ * @argv: points to file name
+ * Return: 0 on success
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+void(argc);
+printf("%s\n", argv[0]);
+return (0);
+}
+
