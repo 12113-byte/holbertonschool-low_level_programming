533310c1875dca1b88bb9cd153bba7d4a66ee7f6
Joleen <12113@holbertonstudents.com>
2025-11-02 10:33:08 +0000

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
