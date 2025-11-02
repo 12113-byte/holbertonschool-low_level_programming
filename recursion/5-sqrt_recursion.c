e03f7b2461301dfa5cee16c1bbedc7e8c81aaeb8
Joleen <12113@holbertonstudents.com>
2025-11-02 06:55:10 +0000

    Task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * support - helps to find the natural square root
+ * @i: number for square root
+ * @n: comparison number, number given
+ */
+
+int support(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return support(n, i + 1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the given number
+ * Return: natural square root on succes, otherwise either 0 or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	return support(n, 1);
+}
+
