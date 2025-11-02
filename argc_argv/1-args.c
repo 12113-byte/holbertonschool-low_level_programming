1d61adab359c353b72f7a2e4d9e8a01dbdcd7ebf
Joleen <12113@holbertonstudents.com>
2025-11-02 10:37:50 +0000

    update Task 1]

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,13 +3,14 @@
 /**
  * main - prints the number of arguments passed into it
  * @argc: number of arguments
+ * @argv: voided
  * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
