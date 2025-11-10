fb4e5a1e9c1832c0ada68c175bbafe160b9e6e74
Joleen <12113@holbertonstudents.com>
2025-11-10 04:45:10 +0000

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: that gets printed
+ * @f: points to a function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+	return;
+	}
+	f(name);
+}
+
