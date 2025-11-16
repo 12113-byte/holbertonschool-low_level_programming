4c005ada63686baa228d04f327e6836d8b1bda94
Joleen <12113@holbertonstudents.com>
2025-11-16 01:04:13 +0000

    header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct printer
+{
+	char *symbol;
+	void (*func)(va_list);
+} printer_t;
+
+void print_a_char(va_list args);
+void print_an_int(va_list args);
+void print_a_float(va_list args);
+void print_a_char_ptr(va_list args);
+
 #endif
