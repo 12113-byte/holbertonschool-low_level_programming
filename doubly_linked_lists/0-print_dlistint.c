8aa6934db0a19d2f6b1c02c636a78a33d5a5c1c0
Joleen <12113@holbertonstudents.com>
2025-11-25 04:59:00 +0000

    Task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of dlistint_t
+ * @h: start of doubly linked list
+ *
+ * Return: number of nodes on sucess
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t len = 0;
+
+	if (current == 0)
+	{
+		return (0);
+	}
+
+	while (current != NULL)
+	{
+		current = current->next;
+		len++;
+	}
+	return (len);
+}
+
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Tasks about doubly linked lists
