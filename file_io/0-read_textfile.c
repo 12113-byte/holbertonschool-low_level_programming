187ab22683e6bbb31e123ba657fc2db59238f04c
Joleen <12113@holbertonstudents.com>
2025-11-30 10:47:45 +0000

    Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,7 +6,8 @@
 /**
  * read_textfile - reads a text file and prints it
  * to the POSIX standard output
- *
+ * @filename: name of the file
+ * @letters: the amount of different files
  * Return: 0 on failure
  */
 
@@ -20,30 +21,24 @@ size_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	buffer = malloc(letters);
-
 	if (!buffer)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
-	
 	if (fd == -1)
 	{
 		return (0);
-	
 	}
 	bytes_read = read(fd, buffer, letters);
-
 	if (bytes_read == -1)
 	{
-		close (fd);
+		close(fd);
 		free(buffer);
 		return (0);
 	}
-	bytes_written = write(1, buffer, bytes_read);
+	bytes_written = write(2, buffer, bytes_read);
 	if (bytes_written != bytes_read)
 	{
 		close(fd);
