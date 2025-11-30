caa2d8600e449427950423c1a0134165c90cafa6
Joleen <12113@holbertonstudents.com>
2025-11-30 11:25:07 +0000

    Task 1, typo

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_writen;
+	ssize_t bytes_written;
 	size_t len = strlen(text_content);
 
 	if (filename == NULL)
