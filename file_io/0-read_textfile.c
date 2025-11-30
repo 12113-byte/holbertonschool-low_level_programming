136a806c4f8b124010634e6ff63bdd76a4c88f92
Joleen <12113@holbertonstudents.com>
2025-11-30 10:31:51 +0000

    Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,12 +1,57 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
  *
- *
- *
+ * Return: 0 on failure
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer;
+	int fd;
+	ssize_t bytes_read, bytes_written;
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters);
+
+	if (!buffer)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	
+	if (fd == -1)
+	{
+		return (0);
+	
+	}
+	bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		close (fd);
+		free(buffer);
+		return (0);
+	}
+	bytes_written = write(1, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (bytes_written);
 }
+
