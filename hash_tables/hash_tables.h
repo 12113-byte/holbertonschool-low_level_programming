9306f99615193af26c512038cadec7a4c280939f
Joleen <12113@holbertonstudents.com>
2025-12-01 05:06:34 +0000

    updated header file, typo

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef HASH_TABLES.H
-#define HASH_TABLES.H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
