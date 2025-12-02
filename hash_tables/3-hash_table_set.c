492966684fa1634368b7dc6458c9bea50778d318
Joleen <12113@holbertonstudents.com>
2025-12-02 04:26:05 +0000

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,21 +13,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	node_t *new_node;
+	hash_node_t *new_node;
+	hash_node_t *current;
+	unsigned long int idx;
 
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 	{
 		return (0);
 	}
-	key_index(key);
-	new_node = malloc(sizeof(node_t));
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[idx];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+
 	if (new_node == NULL)
 	{
-	return (0);
+		return (0);
 	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next =
-
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
 }
 
